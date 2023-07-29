a7306d0e5c3f8af8d61b13189a83a3555e348254
Saetbyeol Lim <saetbyeol701@gmail.com>
2023-07-29 14:08:04 -0400

    mongoDB connection

---
+++ server/config/dbConfig.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.mongo_url);
+
+const connectionResult = mongoose.connection;
+
+connectionResult.on('error', ()=>{
+    console.log('Error connecting to database');
+});
+connectionResult.on('connected', ()=>{
+    console.log('connected to database');
+});
+//module.exports = connectionResult
--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 
+require('dotenv').config();
+
+const dbConfig = require('./config/dbConfig');
+
 const PORT = process.env.PORT || 5000; // 3000 is running 'react app'
 
 app.listen(PORT, ()=>{
     console.log(`Server started on port ${PORT}`);
 });
+
