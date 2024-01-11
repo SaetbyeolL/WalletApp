74a7dee0c54a23cdc3885f37d25eaa9653dbac2a
Saetbyeol Lim <saetbyeol701@gmail.com>
2024-01-11 16:59:57 -0500

    TransactionPage & Transfer API

--- server/server.js
+++ server/server.js
@@ -5,8 +5,10 @@ require("dotenv").config();
 app.use(express.json());
 const dbConfig = require("./config/dbConfig");
 const usersRoute = require("./routes/usersRoute");
+const transactionsRoute = require("./routes/transactionRoute");
 
-app.use('/api/users', usersRoute);
+app.use("/api/users", usersRoute);
+app.use("api/transactions", transactionsRoute);
 
 const PORT = process.env.PORT || 5000; // 3000 is running 'react app'
 
