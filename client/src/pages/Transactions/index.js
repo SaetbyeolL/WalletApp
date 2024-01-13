2ab0556146791bf52e6e946b2f119622538645d6
Saetbyeol Lim <saetbyeol701@gmail.com>
2024-01-12 23:35:51 -0500

    Transfer Funds UI Modal

--- client/src/pages/Transactions/index.js
+++ client/src/pages/Transactions/index.js
@@ -1,73 +1,65 @@
-import React from 'react'
-import PageTitle from '../../components/PageTitle'
-import {Table} from "antd";
+import React from "react";
+import PageTitle from "../../components/PageTitle";
+import { Table } from "antd";
+import TransferFundsModal from "./TransferFundsModal";
 
+function Transactions() {
+  const [showTransferFundsModal, setShowTransferFundsModal] =
+    React.useState(false);
 
-function Transactions(){
-    const columns = [
-        {
-            title:"Date",
-            dataIndex:"date",
-        },
-        {
-            title: "Transaction ID",
-            dataIndex: "TransactionId",
-        },
-        {
-            title: "Amount",
-            dataIndex:"amount",
-        },
-        {
-            title:"Type",
-            dataIndex:"type",
-        },
-        {
-            title:"Reference",
-            dataIndex: "reference",   
-        },
-        {
-            title:"Status",
-            dataIndex:"status",
-        }
-    ];
+  const columns = [
+    {
+      title: "Date",
+      dataIndex: "date",
+    },
+    {
+      title: "Transaction ID",
+      dataIndex: "TransactionId",
+    },
+    {
+      title: "Amount",
+      dataIndex: "amount",
+    },
+    {
+      title: "Type",
+      dataIndex: "type",
+    },
+    {
+      title: "Reference",
+      dataIndex: "reference",
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+    },
+  ];
 
-    return (
-        <div>
-            <div className='flex justify-between items-center'>
-                <PageTitle title='Transactions' />
+  return (
+    <div>
+      <div className="flex justify-between items-center">
+        <PageTitle title="Transactions" />
 
-                <div className='flex gap-1'>
-                    <button className="primary-outlined-btn">Deposit</button>
-                    <button className="primary-contained-btn">Transfer</button>
-                </div>
-            </div>
-
-            <Table columns={columns} dataSource={[]} className='mt-2'/>
+        <div className="flex gap-1">
+          <button className="primary-outlined-btn">Deposit</button>
+          <button
+            className="primary-contained-btn"
+            onClick={() => setShowTransferFundsModal(true)}
+          >
+            Transfer
+          </button>
         </div>
-    );
+      </div>
+
+      <Table columns={columns} dataSource={[]} className="mt-2" />
+
+      {showTransferFundsModal && (
+      <TransferFundsModal 
+        showTransferFundsModal={showTransferFundsModal}
+        setShowTransferFundsModal={setShowTransferFundsModal}
+      />
+      )}
+    </div>
+  );
 }
 
-export default Transactions
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default Transactions;
