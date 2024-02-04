213609ce881f71ffb6bd135d818cb14d5c63190e
Saetbyeol Lim <saetbyeol701@gmail.com>
2024-02-03 23:53:22 -0500

    Deployment setting

--- server/server.js
+++ server/server.js
@@ -13,6 +13,15 @@ app.use("/api/transactions", transactionsRoute);
 app.use("/api/requests", requestsRoute);
 
 const PORT = process.env.PORT || 5000; // 3000 is running 'react app'
+const path = require("path");
+
+// heroku deployment
+if (process.env.NODE_ENV === "production"){
+  app.use(express.static("client/build"));
+  app.get("*", (req, res)=> {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
